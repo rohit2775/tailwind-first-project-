32325484ab7e3ea045d8a7adf6a9d08eea3ca8e4
rohit2775 <rg7558674@gmail.com>
2025-10-04 22:24:09 +0530

    tailwindcss first project

---
+++ my-tailwind-project/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],   // src folder me jo bhi html/js files hain
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
