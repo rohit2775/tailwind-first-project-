529fdc6d63d4146219429ac65cd57f0451c1b848
rohit2775 <rg7558674@gmail.com>
2026-01-12 09:22:32 +0530

    tailwind  learning

--- my-tailwind-project/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],   // src folder me jo bhi html/js files hain
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
